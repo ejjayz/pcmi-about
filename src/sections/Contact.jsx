4711f6d9a328f507c54ed84470155c171422a252
ejjayz <allosochristson@gmail.com>
2024-11-01 19:42:01 +0800

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -67,10 +67,7 @@ const Contact = () => {
     <section className="c-space my-20" id="contact">
       {alert.show && <Alert {...alert} />}
 
-      <div className="relative min-h-screen flex items-center justify-center flex-col">
-        <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
-
-        {/* Globe Section Added Here */}
+{/* Globe Section Added Here */}
         <div className="col-span-1 xl:row-span-4 mb-10">
           <div className="grid-container">
             <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
@@ -93,6 +90,10 @@ const Contact = () => {
             </div>
           </div>
         </div>
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
+
+        
 
         <div className="contact-container">
           <h3 className="head-text">Let's talk</h3>
