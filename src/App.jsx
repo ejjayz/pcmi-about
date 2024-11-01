10c785275f6145041f1c85c567e15a7709b7fd6a
ejjayz <allosochristson@gmail.com>
2024-11-02 00:42:02 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
-import Clients from './sections/Clients.jsx';
 import Projects from './sections/Projects.jsx';
 import WorkExperience from './sections/Experience.jsx';
 
@@ -12,9 +11,8 @@ const App = () => {
     <main className="max-w-7xl mx-auto relative">
       <Navbar />
       <Hero />
-      <About />
+      <About /> {/* This includes the Clients component internally */}
       <Projects />
-      <Clients />
       <WorkExperience />
       <Contact />
       <Footer />
