a834e49bc87354b4c8d0c4c57d3d7207d8e5ff99
ejjayz <allosochristson@gmail.com>
2024-11-11 19:05:31 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
@@ -5,6 +6,7 @@ import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
 import Projects from './sections/Projects.jsx';
 import WorkExperience from './sections/Experience.jsx';
+import Chatbot from './Chatbot'; // Import the new Chatbot component
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
       <WorkExperience />
       <Contact />
       <Footer />
+      <Chatbot /> {/* Add the Chatbot component here */}
     </main>
   );
 };
